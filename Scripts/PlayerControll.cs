0206f0b48d56b0f75c4948f43a8a762a377a85a1
GabrielDoi <33405208+GabrielDoi@users.noreply.github.com>
2018-11-07 11:54:12 -0200

    Add files via upload

---
+++ Scripts/PlayerControll.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PlayerControll : NetworkBehaviour
+{
+    public float velocidade;
+    private GameObject player;
+    private bool fimDeJogo;
+
+    private GameObject textFimDeJogo;
+    private GameObject painelFimDeJogo;
+
+    void Start()
+    {
+        fimDeJogo = false;
+        player = GameObject.FindWithTag("Torta");
+        textFimDeJogo = GameObject.FindWithTag("FimDaPartidaText");
+        painelFimDeJogo = GameObject.FindWithTag("FimDaPartidaPainel");
+
+        if (!isLocalPlayer)
+            return;
+
+        painelFimDeJogo.gameObject.SetActive(false);
+
+        Debug.Log(GetNetworkChannel());
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase.Equals(TouchPhase.Began) && !fimDeJogo)
+            {
+                if (netId.Value == 1)
+                {
+                    CmdFire(velocidade);
+                    
+                }
+                else
+                {
+                    CmdFire(-velocidade);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(netId.Value);
+            if (netId.Value == 1)
+            {
+                CmdFire(velocidade);
+            }
+            else
+            {
+                CmdFire(-velocidade);
+            }
+        }
+
+        if (!fimDeJogo)
+        {
+            VerificarFimDeJogo();
+        }
+    }
+
+    // This [Command] code is called on the Client …
+    // … but it is run on the Server!
+    [Command]
+    void CmdFire(float ve)
+    {
+        if (!isServer)
+            return;
+
+        //Debug.Log("TesteFire-Server");
+        RpcFire(ve);
+    }
+
+    //executado em cada cliente
+    [ClientRpc]
+    void RpcFire(float v)
+    {
+        Debug.Log("cliente teste rpc "+netId.Value+" = " + v);
+
+        player.transform.position += new Vector3(v, 0, 0);
+    }
+
+    void VerificarFimDeJogo()
+    {
+        if (player.transform.localPosition.x >= 1)
+        {
+            if (netId.Value == 1)
+            {
+                Ganhador(netId.Value);
+            }
+            else
+            {
+                Perdedor(netId.Value);
+            }
+        }
+        else
+        {
+            if (player.transform.localPosition.x <= -1)
+            {
+                if (netId.Value == 1)
+                {
+                    Perdedor(netId.Value);
+                }
+                else
+                {
+                    Ganhador(netId.Value);
+                }
+            }
+        }
+    }
+
+    void Ganhador(uint id)
+    {
+        Debug.Log(id + " Eu sou o ganhador!!!");
+        fimDeJogo = true;
+
+        painelFimDeJogo.gameObject.SetActive(true);
+        textFimDeJogo.GetComponent<Text>().text = "Voce Ganhou Parabens!!";
+    }
+
+    void Perdedor(uint id)
+    {
+        Debug.Log(id + " Eu sou o perdedor!!!");
+        fimDeJogo = true;
+
+        painelFimDeJogo.gameObject.SetActive(true);
+        textFimDeJogo.GetComponent<Text>().text = "Voce Perdeu Tente Novamente!!";
+    }
+
+    public void ReiniciarPartida()
+    {
+        Debug.Log("Reiniciou a partida jogador " + netId.Value);
+    }
+
+    public void SairDaPartida()
+    {
+        Debug.Log("Sair da Partida jogador " + netId.Value);
+    }
+
+}
---
+++ Scripts/SeverController.cs
@@ -0,0 +1,295 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class SeverController : NetworkManager
+{
+
+    //referencias dos paineis inicio
+    public Image PainelInicio;
+    public InputField ipDoServidor;
+    public Button conectar;
+    public Button criarServidor;
+
+    //referencias dos paineis criarServidor
+    public Image PainelCriarServidor;
+    public InputField seuIP;
+    public Button cancelarCriarServidor;
+
+    //painel do vuforia
+    public Image PainelScan;
+
+    //public Image PainelMenu;
+    public Image PainelServidor;
+    public Image PainelCliente;
+
+    //painel fimDaPartida
+    public Image PainelFimDaPartida;
+
+    private NetworkClient myClient;
+
+    public Button Sair;
+
+    private GameObject Torta;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //set para o painel criar servidor sumir
+        PainelCriarServidor.gameObject.SetActive(false);
+        PainelScan.gameObject.SetActive(false);
+        PainelServidor.gameObject.SetActive(false);
+        PainelCliente.gameObject.SetActive(false);
+
+        Debug.Log("ok inicializou server");
+
+        //execucao das funcoes de click dos botoes do painel inicio
+        conectar.onClick.AddListener(delegate { conectarAoServer(ipDoServidor.text); });
+        criarServidor.onClick.AddListener(createAServer);
+
+        //execucao das funcoes de click dos botoes do painel criarservidor
+        cancelarCriarServidor.onClick.AddListener(cancelarCriacaoDoServidor);
+
+        //para botao sair
+        Sair.onClick.AddListener(DisconectarESair);
+
+        //get na torta
+        Torta = GameObject.FindWithTag("Torta");
+
+    }
+
+    //funcao para se conectar em um servidor
+    private void conectarAoServer(string ip)
+    {
+        if (ip.Equals(""))
+        {
+            Debug.Log("Digite ip valido Porfavor");
+        }
+        else
+        {
+            Debug.Log("conectar no servidor: " + ip + ":" + networkPort);
+            networkAddress = ip;
+            StartClient();
+            PainelInicio.gameObject.SetActive(false);
+        }
+
+    }
+
+    //funcao para criar um servidor
+    private void createAServer()
+    {
+        //pega endereço ip local da minha maquina para criar um servidor
+        networkAddress = GetIpv4();
+
+        Debug.Log("criar servidor");
+        Debug.Log(networkAddress);
+        seuIP.text = networkAddress;
+        StartHost();
+        PainelInicio.gameObject.SetActive(false);
+        PainelCriarServidor.gameObject.SetActive(true);
+    }
+
+    //funcao para cancelar a criacao do servidor
+    private void cancelarCriacaoDoServidor()
+    {
+        DisconectarESair();
+        StopServer();
+        PainelCriarServidor.gameObject.SetActive(false);
+        PainelInicio.gameObject.SetActive(true);
+
+    }
+
+    private void DisconectarESair()
+    {
+        PainelFimDaPartida.gameObject.SetActive(true);
+        PainelServidor.gameObject.SetActive(false);
+        PainelCliente.gameObject.SetActive(false);
+        Torta.transform.position = new Vector3(0.022f, 0, 0);
+        StopClient();
+    }
+
+    //callbacks do servidor
+    //==================================================================================================
+
+    //detecta o status quando o server for inicializado
+    public override void OnStartServer()
+    {
+        //Output that the Server has started
+        Debug.Log("Server Started!");
+    }
+
+    //detecta o status quando o server for parado
+    public override void OnStopServer()
+    {
+        //Output that the Server has stopped
+        Debug.Log("Server Stopped!");
+    }
+
+    public override void OnServerError(NetworkConnection conn, int errorCode)
+    {
+
+        Debug.Log("Server network error occurred: " + (NetworkError)errorCode);
+
+    }
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+
+        Debug.Log("A client connected to the server: " + conn);
+
+        if(conn.connectionId == 1)
+        {
+            PainelCriarServidor.gameObject.SetActive(false);
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+
+        NetworkServer.DestroyPlayersForConnection(conn);
+
+        if (conn.lastError != NetworkError.Ok)
+        {
+
+            if (LogFilter.logError) { Debug.LogError("ServerDisconnected due to error: " + conn.lastError); }
+
+        }
+
+        Debug.Log("A client disconnected from the server: " + conn);
+
+    }
+
+    public override void OnServerReady(NetworkConnection conn)
+    {
+
+        NetworkServer.SetClientReady(conn);
+
+        Debug.Log("Client is set to the ready state (ready to receive state updates): " + conn);
+
+    }
+
+    // Client callbacks
+    //====================================================================================================
+
+    public override void OnClientConnect(NetworkConnection conn)
+
+    {
+
+        base.OnClientConnect(conn);
+
+        Debug.Log("Connected successfully to server, now to set up other stuff for the client...");
+
+        if(conn.connectionId == 0)
+        {
+            PainelServidor.gameObject.SetActive(true);
+        }
+        else
+        {
+            PainelCliente.gameObject.SetActive(true);
+        }
+        
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+
+        StopClient();
+
+        if (conn.lastError != NetworkError.Ok)
+
+        {
+
+            if (LogFilter.logError) { Debug.LogError("ClientDisconnected due to error: " + conn.lastError); }
+
+        }
+
+        Debug.Log("Client disconnected from server: " + conn);
+
+    }
+
+    public override void OnClientError(NetworkConnection conn, int errorCode)
+    {
+
+        Debug.Log("Client network error occurred: " + (NetworkError)errorCode);
+
+    }
+
+    public override void OnClientNotReady(NetworkConnection conn)
+    {
+
+        Debug.Log("Server has set client to be not-ready (stop getting state updates)");
+
+    }
+
+    public override void OnStartClient(NetworkClient client)
+    {
+
+        Debug.Log("Client has started");
+
+    }
+
+    public override void OnStopClient()
+    {
+
+        Debug.Log("Client has stopped");
+
+    }
+
+    public override void OnClientSceneChanged(NetworkConnection conn)
+    {
+
+        base.OnClientSceneChanged(conn);
+
+        Debug.Log("Server triggered scene change and we've done the same, do any extra work here for the client...");
+
+    }
+    //=====================================================================================================
+    //funcao para pegar o ip local da minha maquina retorna uma string com o ip
+    public static string GetIpv4()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        NetworkInterfaceType _type = NetworkInterfaceType.Wireless80211;
+        string output = "";
+        foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+        {
+            if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+            {
+                foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        output = ip.Address.ToString();
+                    }
+                }
+            }
+        }
+        return output;
+#else
+            return GetIpv4Mobile();
+#endif
+
+
+    }
+    //caso for mobile ele executa essa funcao
+    private static string GetIpv4Mobile()
+    {
+        string deviceHostName = Dns.GetHostName();
+        var ipCount = Dns.GetHostAddresses(deviceHostName);
+        for (int i = 0; i < ipCount.Length; i++)
+        {
+            IPAddress deviceIP = ipCount[i];
+            if (deviceIP.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return deviceIP.ToString();
+            }
+        }
+        return null;
+    }
+}
